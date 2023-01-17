82200b2232112ac950de06f0f07f52029bfab19a
halojenproductions <40622702+halojenproductions@users.noreply.github.com>
2023-01-17 18:55:00 +1300

    Formatting.

--- playwright/pageLoads.spec.ts
+++ playwright/pageLoads.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Page loads', async ({ page }) => {
-  await page.goto('http://localhost:8080/');
-  const banner = await page.getByRole('heading', { name: 'Keep Talking and Nobody Explodes' });
-  await  expect(banner).toBeVisible();
+	await page.goto('http://localhost:8080/');
+	const banner = await page.getByRole('heading', { name: 'Keep Talking and Nobody Explodes' });
+	await expect(banner).toBeVisible();
 });
