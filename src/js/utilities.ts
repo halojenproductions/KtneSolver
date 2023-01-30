f5187fc1b8f871a3931a817142cf71e655d6d5fd
halojenproductions <40622702+halojenproductions@users.noreply.github.com>
2023-01-31 11:56:48 +1300

    Hr hides with solution.

--- src/js/utilities.ts
+++ src/js/utilities.ts
@@ -15,7 +15,21 @@ export function hide(e: HTMLInputElement) {
 	e.classList.add("d-none"); // Hide.
 }
 
-export function getById(id: string): HTMLInputElement{
+export type ModuleId = "Keypad" | "Button";
+export function showSolution(moduleId: ModuleId, visible: boolean = true): void {
+	let targ: NodeListOf<Element> = document.querySelectorAll(`#${moduleId} .solution`);
+
+	targ.forEach(el => {
+		const className = "d-none";
+		if (visible && el.classList.contains(className)) {
+			el.classList.remove(className); // Show.
+		} else if (!visible && !el.classList.contains(className)) {
+			el.classList.add(className); // Hide.
+		}
+	});
+}
+
+export function getById(id: string): HTMLInputElement {
 	return <HTMLInputElement>document.getElementById(id);
 }
 
