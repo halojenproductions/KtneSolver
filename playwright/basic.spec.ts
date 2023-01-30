cbc9203a0c09f5e1fd19253df926d9902ace9e50
anibor164 <sian.mercer+GIT@gmail.com>
2023-01-31 09:41:51 +1300

    Tests
    Fixing basic wire test and removing duplicate test

--- playwright/basic.spec.ts
+++ playwright/basic.spec.ts
@@ -1,12 +1,62 @@
 import { test, expect } from '@playwright/test';
 
+test('yellow, yellow', async ({ page }) => {
+	await page.goto('http://localhost:8080/');
+	const module = await page.locator('#BasicWires .card');
+	await module.locator('label[for="basicwires_1_2"]').click(); //yellow
+	await module.locator('label[for="basicwires_2_2"]').click(); //yellow
+	const answer = await module.locator('#basicwires_solution');
+	await expect(answer).toBeHidden();
+	await expect(answer).toContainText('');
+});
+
 test('yellow, black, yellow', async ({ page }) => {
 	await page.goto('http://localhost:8080/');
-	const module = await page.locator('#basicwires .card');
-	await module.locator('.btn-group > label:nth-child(6)').first().click();
-	await module.locator('div:nth-child(3) > label:nth-child(8)').first().click();
-	await module.locator('div:nth-child(4) > label:nth-child(6)').first().click();
+	const module = await page.locator('#BasicWires .card');
+	await module.locator('label[for="basicwires_1_2"]').click(); //yellow
+	await module.locator('label[for="basicwires_2_3"]').click(); //black
+	await module.locator('label[for="basicwires_3_2"]').click(); //yellow
 	const answer = await module.locator('#basicwires_solution');
 	await expect(answer).toBeVisible();
 	await expect(answer).toContainText('2nd');
+});
+
+test('red, red, blue, blue', async ({ page }) => {
+	await page.goto('http://localhost:8080/');
+	const module = await page.locator('#BasicWires .card');
+	await module.locator('label[for="basicwires_1_0"]').click(); //red
+	await module.locator('label[for="basicwires_2_0"]').click(); //red
+	await module.locator('label[for="basicwires_3_1"]').click(); //blue
+	await module.locator('label[for="basicwires_4_1"]').click(); //blue
+	const answer = await module.locator('#basicwires_solution');
+	await expect(answer).toBeVisible();
+	await expect(answer).toContainText('Serial');
+});
+
+
+test('black, black, black, black, black', async ({ page }) => {
+	await page.goto('http://localhost:8080/');
+	const module = await page.locator('#BasicWires .card');
+	await module.locator('label[for="basicwires_1_3"]').click(); //black
+	await module.locator('label[for="basicwires_2_3"]').click(); //black
+	await module.locator('label[for="basicwires_3_3"]').click(); //black
+	await module.locator('label[for="basicwires_4_3"]').click(); //black
+	await module.locator('label[for="basicwires_5_3"]').click(); //black
+	const answer = await module.locator('#basicwires_solution');
+	await expect(answer).toBeVisible();
+	await expect(answer).toContainText('Serial');
+});
+
+test('yellow, yellow, yellow, yellow, yellow, yellow', async ({ page }) => {
+	await page.goto('http://localhost:8080/');
+	const module = await page.locator('#BasicWires .card');
+	await module.locator('label[for="basicwires_1_2"]').click(); //yellow
+	await module.locator('label[for="basicwires_2_2"]').click(); //yellow
+	await module.locator('label[for="basicwires_3_2"]').click(); //yellow
+	await module.locator('label[for="basicwires_4_2"]').click(); //yellow
+	await module.locator('label[for="basicwires_5_2"]').click(); //yellow
+	await module.locator('label[for="basicwires_6_2"]').click(); //yellow
+	const answer = await module.locator('#basicwires_solution');
+	await expect(answer).toBeVisible();
+	await expect(answer).toContainText('last');
 });
