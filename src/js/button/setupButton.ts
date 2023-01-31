e7acabe0605a2770e16fc61ad6ee508b11deb297
halojenproductions <40622702+halojenproductions@users.noreply.github.com>
2023-01-31 16:19:16 +1300

    Placeholder for setupButton.

---
+++ src/js/button/setupButton.ts
@@ -0,0 +1,13 @@
+/*import { keypad_solve } from './solverKeypad';
+import { buttonDeselect } from '../utilities';
+
+
+export function setupKeypad(): void {
+	Array.from(document.querySelectorAll("#Keypad form .btn")).forEach((element) => {
+		element.addEventListener("click", buttonDeselect);
+		element.addEventListener("click", () => setTimeout(keypad_solve));
+	});
+	// Hook up the clear button so it solves after clearing, thus hiding the solution.
+	document.querySelector("#Keypad button[type='reset']")
+	?.addEventListener("click", () => setTimeout(button_solve));
+}*/
