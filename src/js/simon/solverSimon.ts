deacfbf7c50b594ff8602ace479549435bfa4ad2
anibor164 <sian.mercer+GIT@gmail.com>
2023-02-06 21:52:08 +1300

    Update solverSimon.ts

--- src/js/simon/solverSimon.ts
+++ src/js/simon/solverSimon.ts
@@ -4,12 +4,14 @@ export function simon_solve(): void {
 
 	let result: string[] = [];
 
-	const flash = { //[any vowel colours][no vowels colours]
-		red: [["blue", "yellow", "green"], ["blue", "red", "yellow"]],
-		blue: [["red", "green", "red"], ["yellow", "blue", "green"]],
-		green: [["yellow", "blue", "yellow"], ["green", "yellow", "blue"]],
-		yellow: [["green", "red", "blue"], ["red", "green", "red"]]
-	}
+	const flash = ["red","blue","green","yellow"];
+
+	const button = [ //[any vowel colours][no vowels colours]
+		[["blue", "yellow", "green"], ["blue", "red", "yellow"]], //red
+		[["red", "green", "red"], ["yellow", "blue", "green"]], //blue
+		[["yellow", "blue", "yellow"], ["green", "yellow", "blue"]], //green
+		[["green", "red", "blue"], ["red", "green", "red"]] //yellow
+	]
 
 	let serial_input: NodeListOf<HTMLInputElement> = document.querySelectorAll('.simon_serial:checked');
 	let strikes_input: NodeListOf<HTMLInputElement> = document.querySelectorAll('.simon_strikes:checked');
@@ -33,25 +35,10 @@ export function simon_solve(): void {
 		let html: NodeListOf<HTMLInputElement> = document.querySelectorAll(`.simon_${i}:checked`);
 		if (html.length > 0) {
 			let input: string = html[0].value;
-
-			switch (input) { //TODO: put colour into the array and ref. Remove switch
-				case "red":
-					result.push(flash.red[serial][strikes]);
-					break;
-				case "blue":
-					result.push(flash.blue[serial][strikes]);
-					break;
-				case "yellow":
-					result.push(flash.yellow[serial][strikes]);
-					break;
-				case "green":
-					result.push(flash.green[serial][strikes]);
-					break;
-				default: break;
-			}
+	
+			result.push(button[flash.indexOf(input)][serial][strikes]);
 		}
 	}
-
 
 	let sol: HTMLDivElement = <HTMLDivElement>document.querySelector("#Simon div.solution");
 
