18fed9516a5cdb993764a3a2b8f96d12cebf20db
anibor164 <sian.mercer+GIT@gmail.com>
2023-02-16 20:04:54 +1300

    Update setupComplicated.ts

--- src/js/complicated/setupComplicated.ts
+++ src/js/complicated/setupComplicated.ts
@@ -2,7 +2,7 @@ import { complicated_solve } from './solverComplicated';
 import { buttonDeselect, getById } from '../utilities';
 
 export function setupComplicated(): void {
-	Array.from(document.querySelectorAll("#complicated label.btn")).forEach((element) => {
+	Array.from(document.querySelectorAll("#Complicated label.btn")).forEach((element) => {
 		element.addEventListener("click", buttonDeselect);
 		element.addEventListener("click", (e) => setTimeout(complicated_solve, 0, getById((<HTMLLabelElement>e.target).htmlFor).name));
 	});
