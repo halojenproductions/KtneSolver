2813e5467aa3ac03917bbc0861aad29dc9768712
halojenproductions <40622702+halojenproductions@users.noreply.github.com>
2023-04-29 11:55:18 +1200

    Added vertical spacing.

--- src/js/memory/renderMemory.ts
+++ src/js/memory/renderMemory.ts
@@ -1,6 +1,6 @@
 import { getById } from '../utilities';
 
-export function renderMemory(): void {	
+export function renderMemory(): void {
 	const memoryForm = getById('MemoryForm');
 	const attributes = [
 		{ name: 'display', input: true },
@@ -10,13 +10,16 @@ export function renderMemory(): void {
 
 	for (let i = 1; i <= 5; i++) { //i is the stage
 		const row = document.createElement('div');
-		row.id = `memory_stage_${i}`		
-		attributes.forEach(attribute => { 
+		row.id = `memory_stage_${i}`
+		attributes.forEach(attribute => {
 			const group = document.createElement('div');
 			group.className = 'btn-group btn-group-sm align-top';
+			if (i < 5) {
+				group.classList.add("mb-1");
+			}
 			group.setAttribute('role', 'group');
 			group.style.width = `${100 / (attributes.length)}%`;
-			
+
 
 			if (attribute.input) { //ii is the value
 				for (let ii = 1; ii <= 4; ii++) {
@@ -25,9 +28,9 @@ export function renderMemory(): void {
 					input.id = inputId;
 					input.name = `memory_${attribute.name}_${i}`;
 					input.className = `btn-check memory_${attribute.name}_${i}`;
-					input.setAttribute('type', 'radio');					
+					input.setAttribute('type', 'radio');
 					input.setAttribute('autocomplete', 'off');
-					input.setAttribute('data-elementname', `${attribute.name}`);					
+					input.setAttribute('data-elementname', `${attribute.name}`);
 					input.setAttribute('data-elementstage', `${i}`);
 					input.setAttribute('data-elementvalue', `${ii}`);
 					input.value = ii.toString();
@@ -43,16 +46,16 @@ export function renderMemory(): void {
 				}
 			} else {
 				const label = document.createElement('label');
-				label.className = 'text-primary text-center w-100';
-				label.id = `memory_${attribute.name}_${i}`;				
+				label.className = 'text-primary text-center w-100 pt-1';
+				label.id = `memory_${attribute.name}_${i}`;
 
-				
+
 				group.appendChild(label);
 			}
 
 			row.appendChild(group);
 		});
-		
-		memoryForm.appendChild(row);		
-	}	
+
+		memoryForm.appendChild(row);
+	}
 }
