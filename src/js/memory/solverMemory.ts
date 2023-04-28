b528496bc6f0f1e4973cc2d7549112db0fab4dbb
anibor164 <sian.mercer+GIT@gmail.com>
2023-04-28 12:30:59 +1200

    Clearing form logic
    Adding logic to clean the form.

--- src/js/memory/solverMemory.ts
+++ src/js/memory/solverMemory.ts
@@ -4,27 +4,34 @@ const position = "Position";
 const label = "Label";
 
 
-export function memory_solve(e : Element): void {	
+export function memory_solve(e : HTMLInputElement): void {		
 	clean_form();
 
-	//todo: rewrite this to use this object instead of immediately creating another varible!
+	//only solve if checking a button
+	if (!e.checked) 
+		return;
+	var n = e.getAttribute("data-elementname");
+	var s =  e.getAttribute("data-elementstage");
+	var  v = e.getAttribute("data-elementvalue");
+
+	handle_inputs(n,s,v);
+}
+
+function handle_inputs(name: string, stage: string, value: string): void {	 	 
+	var type : string = "";	
+
 	var element = { 
-		name: e.getAttribute("data-elementname"),
-		stage: e.getAttribute("data-elementstage"),
-		value: e.getAttribute("data-elementvalue"),
-	} 
-	 
-	var type : string = "";
-	
-	var value = element.value != undefined ? parseInt(element.value) : 0; //todo: I guess i'll change this since YOU dont like inline if
-	var stage = element.stage != undefined ? parseInt(element.stage) : 0;
+		name: name,
+		stage: stage != undefined ? parseInt(stage) : 0,
+		value: value != undefined ? parseInt(value) : 0,
+	} 		
 
 	if (element.name === "display")	{	
-		clear_stage(stage);	
+		clear_stage(element.stage);	
 		var number : number = 0;
-		switch (stage) {
+		switch (element.stage) {
 			case 1 : {
-				switch (value) {
+				switch (element.value) {
 						case 1:
 						case 2:
 							type = position;
@@ -44,7 +51,7 @@ export function memory_solve(e : Element): void {
 				break;
 			}
 			case 2: {				
-				switch (value) {
+				switch (element.value) {
 					case 1:						
 						type = label;
 						number = 4;
@@ -70,7 +77,7 @@ export function memory_solve(e : Element): void {
 				break;
 			}
 			case 3: {				
-				switch (value) {
+				switch (element.value) {
 					case 1:
 						type = label;
 						var data = getById("memory_stage_2").getAttribute("data-label");
@@ -106,7 +113,7 @@ export function memory_solve(e : Element): void {
 				break;
 			}
 			case 4: {
-				switch (value) {
+				switch (element.value) {
 					case 1:
 						type = position;
 						var data = getById("memory_stage_1").getAttribute("data-position");
@@ -138,7 +145,7 @@ export function memory_solve(e : Element): void {
 				break;
 			}
 			case 5: {				
-				switch (value) {
+				switch (element.value) {
 					case 1:
 						type = label;
 						var data = getById("memory_stage_1").getAttribute("data-label");
@@ -186,48 +193,54 @@ export function memory_solve(e : Element): void {
 			}
 		}	
 
-		data_persistance(stage, type, number);
-		getById(`memory_label_${stage}`).appendChild(document.createTextNode(`${type} ${number}`));
+		data_persistance(element.stage, type, number);
+		getById(`memory_label_${element.stage}`).appendChild(document.createTextNode(`${type} ${number}`));
 
 	} else if (element.name === "input") {
 		//maybe also retrieve the selected display value to determin stuff?
 		//clear data attributes
-		//STOP WORKING ON THIS AND MAKE A PLAN
-		var array = getById(`memory_stage_${stage}`).attributes;
+		debugger
+		var array = getById(`memory_stage_${element.stage}`).attributes;
 				
 		const data = array[1].name;
 		if(data.includes(label.toLowerCase())) {
-			data_persistance(stage, position, value);			
+			data_persistance(element.stage, position, element.value);			
 		}
 		else if(data.includes(position.toLowerCase())) {
-			data_persistance(stage, label, value);	
-			
+			data_persistance(element.stage, label, element.value);			
 		}	
+
+		//repeat display logic?
 	}
 }
 
 function data_persistance(stage : number, type : string, number : number) {
 	var element = getById(`memory_stage_${stage}`);
-	element.setAttribute(`data-${type}`, number.toString())
+	element.setAttribute(`data-${type.toLowerCase()}`, number.toString())
 }
 
-function clear_stage(stage : number) {
-	//todo: clear attributes
-	getById(`memory_label_${stage}`).replaceChildren(""); //don't tell the courts I wrote this code!
+function clear_stage(stage : number) {	
+	getById(`memory_label_${stage}`).replaceChildren("");
 	var inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll(`.memory_input_${stage}`);
 	if (inputs != null) {
 		inputs.forEach((element) => {
 			element.checked = false;
-		});
-		
+		});		
 	}
+
+	var persistance = getById(`memory_stage_${stage}`)
+
+	persistance.removeAttribute("data-label")
+	persistance.removeAttribute("data-position")
 }
 
 function clean_form() {
-	//check any missing data
-	//check all inputs have matching display else all clear stage
-	//check all labels have mathcing display else call clear stage
-	//check all attributes match inputs and display	
-	throw new Error('Function not implemented.');
-}
+	for (let i = 1; i <= 5; i++) { 
+			var display = document.querySelector(`.memory_display_${i}:checked`)
+			if (display == undefined) {
+				clear_stage(i);
+			}
+		}		
+	}	
+
 	
