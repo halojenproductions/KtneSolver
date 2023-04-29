8cafb53de8100996e59b92fc1594bada7cd30418
halojenproductions <40622702+halojenproductions@users.noreply.github.com>
2023-04-29 23:24:54 +1200

    Fixed attribute alignment.

--- src/js/memory/solverMemory.ts
+++ src/js/memory/solverMemory.ts
@@ -7,9 +7,9 @@ interface ElementData {
 	name: string | null;
 	stage: number;
 	value: number;
-  }
+}
 
-export function memory_solve(e : HTMLInputElement): void {		
+export function memory_solve(e: HTMLInputElement): void {
 	clean_form();
 
 	const element: ElementData = {
@@ -20,12 +20,12 @@ export function memory_solve(e : HTMLInputElement): void {
 
 	if (element.name && element.stage && element.value) {
 
-		if (element.name === "display")	{	
+		if (element.name === "display") {
 			handle_display(element);
 
 		} else if (element.name === "input") {
-			handle_inputs(element);	
-					
+			handle_inputs(element);
+
 			//clears any inputs above this stage, and any labels that are affected by this change
 			refresh_labels(element.stage);
 		}
@@ -33,102 +33,96 @@ export function memory_solve(e : HTMLInputElement): void {
 }
 
 function handle_inputs(element: ElementData) {
-	var array = getById(`memory_stage_${element.stage}`).attributes;
-
-	const data = array[1].name;
-	if (data.includes(label.toLowerCase())) {
+	let row = getById(`memory_stage_${element.stage}`);
+	if (row.getAttribute("data-label")) {
 		data_persistance(element.stage, position, element.value);
-	}
-	else if (data.includes(position.toLowerCase())) {
+	} else if (row.getAttribute("data-position")) {
 		data_persistance(element.stage, label, element.value);
 	}
 }
 
 function handle_display(element: ElementData) {
-	clear_stage(element.stage);	
-	var number : number = 0;
-	var type : string = "";	
+	clear_stage(element.stage);
+	var number: number = 0;
+	var type: string = "";
 	switch (element.stage) {
-		case 1 : {
+		case 1: {
 			switch (element.value) {
-					case 1:
-					case 2:
-						type = position;
-						number = 2;
-						break;			
-					case 3:
-						type = position;
-						number = 3;
-						break;				
-					case 4:
-						type = position;
-						number = 4;
-						break; 
-					default:
-						break;				
-				};	
+				case 1:
+				case 2:
+					type = position;
+					number = 2;
+					break;
+				case 3:
+					type = position;
+					number = 3;
+					break;
+				case 4:
+					type = position;
+					number = 4;
+					break;
+				default:
+					break;
+			};
 			break;
 		}
-		case 2: {				
+		case 2: {
 			switch (element.value) {
-				case 1:						
+				case 1:
 					type = label;
 					number = 4;
-					break;	
+					break;
 				case 2:
-				case 4:						
+				case 4:
 					type = position;
 					var data = getById("memory_stage_1").getAttribute("data-position");
-					if (data == undefined)
-					{
+					if (data == undefined) {
 						console.log("stage 1 missing data");
 						return;
 					}
 					number = parseInt(data);
-					break;	
+					break;
 				case 3:
 					type = position;
 					number = 1;
 					break;
 				default:
-					break;					
-			};	
+					break;
+			};
 			break;
 		}
-		case 3: {				
+		case 3: {
 			switch (element.value) {
 				case 1:
 					type = label;
 					var data = getById("memory_stage_2").getAttribute("data-label");
-					if (data == undefined)
-					{
+					if (data == undefined) {
 						console.log("stage 2 missing data");
 						return;
 					}
-					number = parseInt(data);						
-					break;							
-				case 2:	
+					number = parseInt(data);
+					break;
+				case 2:
 					type = label;
 					var data = getById("memory_stage_1").getAttribute("data-label");
-					
-					if (data == undefined)
-					{
+
+					if (data == undefined) {
 						console.log("stage 1 missing data");
 						return;
 					}
 					number = parseInt(data);
-					break;	
+					break;
 				case 3:
 					type = position;
 					number = 3;
 					break;
-				case 4:	
+				case 4:
 					type = label;
 					number = 4;
 					break;
 				default:
-					break;					
-			};							
+					break;
+			};
 			break;
 		}
 		case 4: {
@@ -136,98 +130,92 @@ function handle_display(element: ElementData) {
 				case 1:
 					type = position;
 					var data = getById("memory_stage_1").getAttribute("data-position");
-					if (data == undefined)
-					{
+					if (data == undefined) {
 						console.log("stage 1 missing data");
 						return;
 					}
 					number = parseInt(data);
-					break;	
-				case 2:	
+					break;
+				case 2:
 					type = position;
-					number = 1;							
-					break;	
+					number = 1;
+					break;
 				case 3:
-				case 4:	
+				case 4:
 					type = position;
 					var data = getById("memory_stage_2").getAttribute("data-position");
-					if (data == undefined)
-					{
+					if (data == undefined) {
 						console.log("stage 2 missing data");
 						return;
 					}
 					number = parseInt(data);
 					break;
 				default:
-					break;					
-			};			
+					break;
+			};
 			break;
 		}
-		case 5: {				
+		case 5: {
 			switch (element.value) {
 				case 1:
 					type = label;
 					var data = getById("memory_stage_1").getAttribute("data-label");
-					if (data == undefined)
-					{
+					if (data == undefined) {
 						console.log("stage 1 missing data");
 						return;
 					}
 					number = parseInt(data);
-					break;	
-				case 2:	
+					break;
+				case 2:
 					type = label;
 					var data = getById("memory_stage_2").getAttribute("data-label");
-					if (data == undefined)
-					{
+					if (data == undefined) {
 						console.log("stage 2 missing data");
 						return;
 					}
 					number = parseInt(data);
-					break;		
+					break;
 				case 3:
 					type = label;
 					var data = getById("memory_stage_4").getAttribute("data-label");
-					if (data == undefined)
-					{
+					if (data == undefined) {
 						console.log("stage 4 missing data");
 						return;
 					}
 					number = parseInt(data);
-					break;	
-				case 4:	
+					break;
+				case 4:
 					type = label;
 					var data = getById("memory_stage_3").getAttribute("data-label");
-					if (data == undefined)
-					{
+					if (data == undefined) {
 						console.log("stage 3 missing data");
 						return;
 					}
 					number = parseInt(data);
 					break;
 				default:
-					break;					
-			};			
+					break;
+			};
 			break;
 		}
-	}	
+	}
 
 	data_persistance(element.stage, type, number);
 	getById(`memory_label_${element.stage}`).appendChild(document.createTextNode(`${type} ${number}`));
 }
 
-function data_persistance(stage : number, type : string, number : number) {
+function data_persistance(stage: number, type: string, number: number) {
 	var element = getById(`memory_stage_${stage}`);
 	element.setAttribute(`data-${type.toLowerCase()}`, number.toString())
 }
 
-function clear_stage(stage : number) {	
+function clear_stage(stage: number) {
 	getById(`memory_label_${stage}`).replaceChildren("");
 	var inputs: NodeListOf<HTMLInputElement> = document.querySelectorAll(`.memory_input_${stage}`);
 	if (inputs != null) {
 		inputs.forEach((element) => {
 			element.checked = false;
-		});		
+		});
 	}
 
 	var persistance = getById(`memory_stage_${stage}`)
@@ -237,26 +225,25 @@ function clear_stage(stage : number) {
 }
 
 function clean_form() {
-	for (let i = 1; i <= 5; i++) { 
+	for (let i = 1; i <= 5; i++) {
 		var display = document.querySelector(`.memory_display_${i}:checked`)
 		if (display == undefined) {
 			clear_stage(i);
 		}
-	}		
-}	
+	}
+}
 
-function refresh_labels(currentStage : number) {
-	for (let i = currentStage + 1; i <= 5; i++) { 		
+function refresh_labels(currentStage: number) {
+	for (let i = currentStage + 1; i <= 5; i++) {
 		const display: HTMLInputElement | null = document.querySelector(`.memory_display_${i}:checked`);
 		if (display) {
 			let element: ElementData = {
 				name: 'display',
 				stage: i,
 				value: parseInt(display.value)
-			};		
-			
+			};
+
 			handle_display(element);
 		}
 	}
 }
-	
