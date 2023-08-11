f8365cdc39ccf1e79f789a9a53d4859bef0c85bb
anibor164 <sian.mercer+GIT@gmail.com>
2023-08-11 21:23:09 +1200

    Update renderWords.ts

--- src/js/words/renderWords.ts
+++ src/js/words/renderWords.ts
@@ -25,61 +25,50 @@ function createWordElement(stage: number, key: string, value: any) {
 export function renderWords(): void {
 	var words_inputs = getById("WordsForm");
 
-	let group = document.createElement("div");
-
-	const filteredEntriesStage1 = Object.entries(wordsDictionary)
-		.filter(([_, value]) => value.description !== "")
-		.sort((a, b) => a[0].localeCompare(b[0]));
-
-
-	filteredEntriesStage1.forEach(([key, value], index) => {
-		const [input, label] = createWordElement(1, key, value);
-
-		group.setAttribute("data-stage", "1");
-		group.setAttribute("role", "group");
-		group.className = "btn-group btn-group-sm d-flex";
-
-		group.appendChild(input);
-		group.appendChild(label);
-
-		if (++index % RowWrap === 0 || index === filteredEntriesStage1.length) {
-			words_inputs.appendChild(group);
-			group = document.createElement("div");
-
-		}
-	});
+	createGroupElement(words_inputs, 1);
 
 	var stage2group = document.createElement("div");
 	stage2group.id = "stage2";
 
-	group = document.createElement("div");
-
-
-	var stage1Solution = document.createElement("div");
-	stage1Solution.className = "text-primary";
-	stage1Solution.id = "stage1Solution"
-	stage2group.appendChild(stage1Solution);
-
-	const filteredEntriesStage2 = Object.entries(wordsDictionary)
-		.filter(([_, value]) => value.words !== "")
-		.sort((a, b) => a[0].localeCompare(b[0]));
-
-	filteredEntriesStage2.forEach(([key, value], index) => {
-		const [input, label] = createWordElement(2, key, value);
-
-		group.setAttribute("role", "group");
-		group.className = "btn-group btn-group-sm d-flex stage2";
-
-		group.appendChild(input);
-		group.appendChild(label);
-
-		if (++index % RowWrap === 0 || index === filteredEntriesStage2.length) {
-			stage2group.appendChild(group);
-			group = document.createElement("div");
-		}
-	});
+	createGroupElement(stage2group, 2);
 	words_inputs.appendChild(stage2group);
 
 	const stage2Section = getById("stage2")
 	hide(stage2Section);
 }
+
+function createGroupElement(div: HTMLDivElement, stage: number) {
+	let group = document.createElement("div");
+
+	if (stage == 2) {
+		var solution = document.createElement("div");
+		solution.className = "text-primary";
+		solution.id = "stage1Solution";
+		div.appendChild(solution);
+	}
+
+	const filteredEntries = Object.entries(wordsDictionary)
+		.filter(([_, value]) => (stage === 1 ? value.description !== "" : stage === 2 ? value.words !== "" : false))
+		.sort((a, b) => a[0].localeCompare(b[0]));
+
+
+	filteredEntries.forEach(([key, value], index) => {
+		const [input, label] = createWordElement(stage, key, value);
+
+		group.setAttribute("data-stage", `${stage}`);
+		group.setAttribute("role", "group");
+		group.className = `btn-group btn-group-sm  d-flex stage${stage}`;
+
+		group.appendChild(input);
+		group.appendChild(label);
+
+		if (++index % RowWrap === 0 || index === filteredEntries.length) {
+			div.appendChild(group);
+			group = document.createElement("div");
+
+		}
+	});
+	return group;
+}
+
+
