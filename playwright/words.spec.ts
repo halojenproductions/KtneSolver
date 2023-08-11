26b216415f6c3f4fd8ce953e95e3d10829d92775
anibor164 <sian.mercer+GIT@gmail.com>
2023-08-11 23:16:42 +1200

    Update words.spec.ts

--- playwright/words.spec.ts
+++ playwright/words.spec.ts
@@ -47,18 +47,7 @@ test('step 2', async ({ page }) => {
 	await module.locator('label[for="words_stage2_ready"]').click();
 	await expect(answer).toContainText('Yes, Okay, What, Middle, Left, Press, Right, Blank, Ready');
 
-	await module.locator('label[for="words_stage2_first"]').click();
-	await expect(answer).toContainText('Left, Okay, Yes, Middle, No, Right, Nothing, U-H-H-H, Wait, Ready, Blank, What, Press, First');
-
-	await module.locator('label[for="words_stage2_no"]').click();
-	await expect(answer).toContainText('Blank, U-H-H-H, Wait, First, What, Ready, Right, Yes, Nothing, Left, Press, Okay, No');
-
 	await module.locator('label[for="words_stage2_blank"]').click();
 	await expect(answer).toContainText('Wait, Right, Okay, Middle, Blank');
 
-	await module.locator('label[for="words_stage2_nothing"]').click();
-	await expect(answer).toContainText('U-H-H-H, Right, Okay, Middle, Yes, Blank, No, Press, Left, What, Wait, First, Nothing');
-
-	await module.locator('label[for="words_stage2_yes"]').click();
-	await expect(answer).toContainText('Okay, Right, U-H-H-H, Middle, First, What, Press, Ready, Nothing, Yes');
 });
